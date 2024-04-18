ef45c9bca363424c8be9bccd2adc6abb90af55a4
Beyla Irakoze <beylaruz@gmail.com>
2024-04-18 16:10:24 +0200

    Special outputs for multiples of 3 and 5

---
+++ 7 Kyu/fizzBuzz.js
@@ -0,0 +1,21 @@
+
+function fizzbuzz(n){
+  var array = [];
+  for (let i = 1; i<=n; i++){
+    if( i % 3 === 0 && i % 5 === 0){
+      array.push("FizzBuzz")
+    }
+    else if(i % 3 === 0){
+        array.push("Fizz")
+  }
+    else if(i % 5 === 0){
+        array.push("Buzz")
+      }
+    else{
+        array.push(i)
+    }
+}  
+return array;
+  }
+
+ console.log(fizzbuzz(100))
