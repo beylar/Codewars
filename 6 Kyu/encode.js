819ae8339363e753c9c6c22d8ca6020325caddc8
Beyla Irakoze <beylaruz@gmail.com>
2024-05-02 17:56:09 +0200

    Encode and Decode

---
+++ 6 Kyu/encode.js
@@ -0,0 +1,16 @@
+function encode(string){
+    // string = [...string]
+    // for(let i=0;i<string.length; i++){
+    //     if(vowels.includes(string[i])){
+    //       string[i]= vowels.indexOf( string[i])+ 1
+    //     }
+    // }
+
+  return [...string].map(el => {return ( "aeiou".includes(el))?  "aeiou".indexOf(el) + 1 : el}).join('')}
+
+  function decode(string){
+    return [...string].map(el => {return ("12345".includes(el)) ? {'1':"a","2":"e","3":"i","4":"o","5":"u"}[el] : el}).join('')
+  }
+
+console.log(encode('hello'))
+console.log(decode('h2ll4'))
