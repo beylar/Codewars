5ea4792caf6ecf978334adce956c3ceff60d4741
Beyla Irakoze <beylaruz@gmail.com>
2024-05-03 14:44:29 +0200

    This keyword

--- 8 Kyu/this.js
+++ 8 Kyu/this.js
@@ -4,7 +4,16 @@ function NameMe(first, last) {
     this.name = this.firstName + ' ' + this.lastName;
 }
 //return in a constructor function changes the reference of the object only to the returned value
-let n = new NameMe('John', 'Doe');
+
+class Name{
+    constructor(first,last){
+        this.firstName = first;
+        this.lastName = last;
+        this.name = this.firstName + ' ' + this.lastName;
+    }
+}
+
+let n = new Name('John', 'Doe');
 console.log(n.firstName) //Expected: John
 console.log(n.lastName) //Expected: Doe
 console.log(n.name) //Expected: John Doe
