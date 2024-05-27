b6ff6147cefa07f8375bdceaab786229460bf812
Beyla Irakoze <beylaruz@gmail.com>
2024-05-27 20:10:12 +0200

    Optimizing

--- 6 Kyu/output.js
+++ 6 Kyu/output.js
@@ -5,7 +5,7 @@
 // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 
 //increments can't work with for in....
-function whoLikes(array){
+const whoLikes = (array) => {
     let count= 0;
     for(let i=0; i<array.length;i++ ){
         if(array.length == 1){
