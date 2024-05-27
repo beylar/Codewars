17afc3f3eb7dfee222aa897310dd9c4df60f3683
Beyla Irakoze <beylaruz@gmail.com>
2024-05-27 20:02:59 +0200

    three tests for those who like this

---
+++ 6 Kyu/output.js
@@ -0,0 +1,31 @@
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+//increments can't work with for in....
+function whoLikes(array){
+    let count= 0;
+    for(let i=0; i<array.length;i++ ){
+        if(array.length == 1){
+            return `${array[i]} likes this`
+        }
+        if(array.length == 2){
+            return `${array[i]} and ${array[i+1]} like this`
+        }
+        if(array.length == 3){
+            return `${array[i]}, ${array[i+1]} and ${array[i+2]} like this`
+        }
+        // if(array.length >= 4 ){
+        //     return 
+        // }
+    }
+    return `no one like this`
+
+}
+
+console.log(whoLikes([]))
+console.log(whoLikes(["Peter"]))
+console.log(whoLikes(["Jacob", "Alex"]))
+console.log(whoLikes(["Jacob", "Alex", "Peter"]))
