d2f3eeb9a2598fc600c704275848b006e14a29af
Beyla Irakoze <beylaruz@gmail.com>
2024-05-30 19:35:19 +0200

    counting electrons on a shell

---
+++ 6 Kyu/electron.js
@@ -0,0 +1,17 @@
+// You are a khmmadkhm scientist and you decided to play with electron distribution among atom's shells. 
+// You know that basic idea of electron distribution is that electrons should fill a shell until it's 
+// holding the maximum number of electrons.
+
+function atomicNumber(num){
+    //"What are we doing today, Brain?" "The same thing we did last week, Pinky... Take over the CodeWars"
+    let atomic = [];
+    
+
+
+    return atomic
+  }
+
+console.log(atomicNumber(11))
+console.log(atomicNumber(1))
+console.log(atomicNumber(22))
+console.log(atomicNumber(47))
