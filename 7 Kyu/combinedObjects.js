65f205ee24b85bd4c5bd03712c1a3b6ba4f19460
Beyla Irakoze <beylaruz@gmail.com>
2024-06-05 15:50:34 +0200

    combined objects

--- 7 Kyu/combinedObjects.js
+++ 7 Kyu/combinedObjects.js
@@ -3,20 +3,28 @@
 // combine(objA, objB)
 //Returns { a: 13, b: 20, c: 36, d: 3 }
 
-let combinedObjects = (objA, objB) => {
-    let newObj = {}
-    for(keys in objA){
-        for(key in objB){
-            if(keys == key){
-                newObj[keys] = Number(objA[keys] + Number(objB[key]))
-                //newObj[key] = Number(objA[keys]) + Number(objB[key])
+let combinedObjects = (objA, ...restObjectArgs) => {
+    let newObj = {...objA}
+    // for(keys in objA){
+    //     for(key in objB){
+    //         if(keys == key){
+    //             newObj[keys] = Number(objA[keys] + Number(objB[key]))
+    //             //newObj[key] = Number(objA[keys]) + Number(objB[key])
+    //         }
+    //         if(keys != key){
+    //             Object.assign(newObj, Number(objA[keys]), Number(objB[key]))
+    //         }
+    //     }
+    // }
+
+    restObjectArgs.forEach(obj => {
+        for (key in obj) {
+            (newObj.hasOwnProperty(key) && typeof obj[key] === 'number') ?
+                newObj[key] += obj[key] : 
+                newObj[key] = obj[key]
             }
-            if(keys != key){
-                newObj[keys] = Number(objA[keys])
-                newObj[key] = Number(objB[key])
-            }
-        }
-    }
+        });
+    
     return newObj;
 }
 console.log(combinedObjects({ a: 10, b: 20, c: 30 },{ a: 3, c: 6, d: 3 }))
