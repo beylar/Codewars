cada8c6e8acd5055f20d55163cbf62bae252dbf5
Beyla Irakoze <beylaruz@gmail.com>
2024-06-11 16:30:25 +0200

    reverse strings in an array that are on an even index

--- 6 Kyu/reverseString.js
+++ 6 Kyu/reverseString.js
@@ -1,17 +1,11 @@
 // reverse("Reverse this string, please!"), "Reverse siht string, !esaelp"
-function reverse(str){
-   str = str.split(" ");
-   let array = []
-   str.map((el) => {
-    if(el == "Reverse" || el == "Reversing" || el == "reverse" || el == "reversing" || el == "reversed" || el == "Reversed"){
-        array.push(el)
+function reverse(str) {
+    let words = str.trim().split(/\s+/);
+    for (let i = 0; i < words.length; i++) {
+        if (i % 2 !== 0) {
+            words[i] = words[i].split("").reverse().join("");
+        }
     }
-    else{
-    let reversed = el.split("").reverse().join("")
-    console.log(reversed)
-    array.push(reversed)
+    return words.join(" ");
 }
-   })
-return array.join(" ")
-  }
   console.log(reverse("Reverse this string, please!"))
