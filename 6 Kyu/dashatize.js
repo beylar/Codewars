afc7e3b75edaa8a5a600d74759f58e384fe445dd
Beyla Irakoze <beylaruz@gmail.com>
2024-06-27 17:29:43 +0200

    put dashes on the numbers before and after an odd number

--- 6 Kyu/dashatize.js
+++ 6 Kyu/dashatize.js
@@ -1,40 +1,30 @@
 function dashatize(num) {
-    let str = String(num).split("")
-    let result = ""
-    if(str.length === 1){
-      return str.join("")
-   }
-    else if(str.length > 1){
-    for(let i = 0; i< str.length; i++){
-      if(str[i] % 2 !== 0){
-        if(str[i+1] % 2 !== 0 && str[i-1] !== 0){
-          if(i === 0){
-            result += `${str[i]}-`
-          }
-          else{
-            
-          result += `${str[i]}`
-          }
+  let str = Math.abs(num).toString().split("");
+  let result = "";
+
+  if (str.length === 1) {
+    return str.join("");
+  } else {
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] % 2 !== 0) {
+        if (i === 0) {
+          result += `${str[i]}-`;
+        } else if (i === str.length - 1) {
+          result += `-${str[i]}`;
+        } else {
+          result += `-${str[i]}-`;
         }
-        else if(str[i-1] % 2 !== 0){
-          result += `${str[i]}-`
-        }
-        else{
-        result += `-${str[i]}-`
-        }
-        
-      }
-      else{
-        result += str[i]
+      } else {
+        result += str[i];
       }
     }
-  return result
-      }
+    return result.replace(/--+/g, '-').replace(/^-|-$/g, '');
   }
-  
+}
   console.log(dashatize(5311))
   
   //change the numbers into strings
   //iterate through a string and if the string is odd add - before and after it
   //return the string with the dashes
   // ?? should it be an array in order to be to add the - or a string can 
+  // use regex to replace in the places where the - are consecutive 
