22d342e02925cd4ebe1ef3286be3d644d912be34
Beyla Irakoze <beylaruz@gmail.com>
2024-07-23 15:53:51 +0200

    check the parity and the number of 1 in the provided binary number

--- 6 Kyu/parity_bit.js
+++ 6 Kyu/parity_bit.js
@@ -1,16 +1,16 @@
 function checkParity(parity, bin){
-    let value = parseInt(bin, 2)
-    isEven = value % 2 === 0
-    if(parity === "even") return isEven ? 1 : 0
-    else if(parity === "odd") return isEven ? 0 : 1
-  }
-  
+  let value = bin.split("").filter(bit => bit === '1').length
+  let isEven = value % 2 === 0
+  if(parity === "even") {
+    return isEven ? 0 : 1 }
+  else if(parity === "odd"){
+    return isEven ? 1 : 0
+    }
+}
   console.log(checkParity('even',101010))//1
   console.log(checkParity('odd',101010))//0
   console.log(checkParity('even',101011))//1
   
-  
-  
-  //change from binary to decimal
-  //check if the value is odd or even 
-  //check what to add to turn it into odd
+//split the given number and count the number of 1 to a sum to even or odd
+//then check if the parity is even add 0 else 1
+//then check if the parity is odd add 1 else 0
