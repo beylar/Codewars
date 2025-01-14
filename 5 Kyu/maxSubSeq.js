11567728497bec96b554d8b56758d5c5e64f5d37
Beyla Ruzindana Irakoze <142901020+beylar@users.noreply.github.com>
2025-01-13 21:17:01 -0500

    Maximum subarray sum

---
+++ 5 Kyu/maxSubSeq.js
@@ -0,0 +1,15 @@
+var maxSequence = function(arr) {
+    if (arr.length === 0) return 0; 
+  
+    let maxSum = 0; 
+    let currentSum = 0; 
+  
+    for (let num of arr) {
+      currentSum = Math.max(0, currentSum + num); 
+      maxSum = Math.max(maxSum, currentSum); 
+    }
+  
+    return maxSum;
+  };
+  
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])) // Output: 6
